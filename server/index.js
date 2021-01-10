78a85ad2191c767630eb8219576e48c99d652d81
pohern <ohern_p@yahoo.com>
2021-01-10 08:15:34 -0600

    fix issue with cors being defined after routes

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ lmit: "30mb", extended: "true" }));
 app.use(cors());
+
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL =
   "mongodb+srv://patrickohern:generalassembly@patrick-cluster.hkabu.mongodb.net/drinks?retryWrites=true&w=majority";
