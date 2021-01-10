9505c70665ff26a3eb131e8c6c865fde71df7876
pohern <ohern_p@yahoo.com>
2021-01-10 09:15:19 -0600

    update index.js

--- server/index.js
+++ server/index.js
@@ -14,8 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL =
-  "mongodb+srv://patrickohern:generalassembly@patrick-cluster.hkabu.mongodb.net/drinks?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
