57021d98944c0a2d003d6bd0023517844ba7ea79
pohern <ohern_p@yahoo.com>
2021-01-14 13:56:14 -0600

    project complete

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
+import { useDispatch, useSelector } from "react-redux";
 import useStyles from "./styles";
+import { createPost, updatePost } from "../../actions/posts";
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     creator: "",
     title: "",
@@ -11,21 +13,45 @@ const Form = () => {
     tags: "",
     selectedFile: "",
   });
+  const post = useSelector((state) => currentId ? state.posts.find((p)=> p._id === currentId): null);
+  const dispatch = useDispatch();
   const classes = useStyles();
 
-  const handleSubmit = () => {};
+  useEffect(()=>{
+    if(post) setPostData(post)
+  },[post])
 
-  const clear = () => {}
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (currentId) {
+      dispatch(updatePost(currentId, postData));
+    } else {
+      dispatch(createPost(postData));
+    }
+    clear()
+  };
+
+  const clear = () => {
+      setCurrentId(null)
+      setPostData(
+        {creator: "",
+        title: "",
+        message: "",
+        tags: "",
+        selectedFile: ""}
+      );
+  };
 
   return (
     <Paper className={classes.paper}>
       <form
         autoComplete='off'
         noValidate
-        className={classes.form}
+        className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
-        <Typography variant='h6'>Creating a Memory</Typography>
+        <Typography variant='h6'>{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
         <TextField
           name='creator'
           variant='outlined'
@@ -60,7 +86,7 @@ const Form = () => {
           label='Tags'
           fullWidth
           value={postData.tags}
-          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}
         />
         <div className={classes.fileInput}>
           <FileBase
--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config()
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
@@ -17,8 +19,7 @@ app.use('/posts', postRoutes)
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port:${PORT}`)))
   .catch((error) => console.log(error.message));
 
